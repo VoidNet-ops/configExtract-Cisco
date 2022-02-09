199f1a3d4e72254bd548c28138ce34508b650ae1
Dennis Ozmert <94586639+dozmert@users.noreply.github.com>
2022-02-09 13:21:32 +1000

    reversal

--- configExtract-Cisco/configExtract-Cisco.py
+++ configExtract-Cisco/configExtract-Cisco.py
@@ -18,7 +18,7 @@
 .NOTES
   Version:        3.03
   Author:         Dennis Ozmert
-  GitHub:         https://github.com/VoidNet-ops
+  GitHub:         https://github.com/dozmert
   Creation Date:  27/09/2021 @ 9:00am
   Last Updated:   27/10/2021
   Purpose/Change: Device config backup
